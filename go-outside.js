1cd6419f4f004bd05957e9df2e3c354074337a3e
Christopher Sawala <chris.sawala@gmail.com>
2016-09-30 01:21:07 +0000

    Add conditional exercises

---
+++ go-outside.js
@@ -0,0 +1,20 @@
+// var raining = true;
+// var cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// }
+
+// console.log("Now you're ready to go outside!");
+
+var cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
